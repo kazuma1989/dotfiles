cb0d2757450540718ed3ae60f700116bc750b136
kazuma1989 <kazuma.ebina@quipper.com>
2022-02-01 23:14:34 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env zsh
 
+# Dependency of Git and Homebrew
+xcode-select --install
+
 # Clone
-git clone https://github.com/kazuma1989/dotfiles.git ~/dotfiles
+git clone https://github.com/kazuma1989/dotfiles.git ~/.dotfiles
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle --file ~/.dotfiles/Brewfile
 
 # Backup. Rename xxx -> xxx.orig if it exists.
 mv ~/.config{,.orig}
-ln -s ~/dotfiles/.config ~/.config
+ln -s ~/.dotfiles/.config ~/.config
 
 mv ~/.gitconfig{,.orig}
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 mv ~/.vimrc{,.orig}
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 mv ~/.zshenv{,.orig}
-ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
 
 mv ~/.zshrc{,.orig}
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-
-# Homebrew
-xcode-select --install # dependency
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew bundle --file ~/dotfiles/Brewfile
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
